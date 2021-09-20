c12211669c63d8669f38954029367ab2e4038c59
DireWolf707 <rahulsood707@gmail.com>
2021-09-20 15:37:54 +0530

    added column feature metrics

--- fancylit/stats/data_stats.py
+++ fancylit/stats/data_stats.py
@@ -20,3 +20,15 @@ def df_describe(
     )
 
     st.write(df.describe())
+
+
+def show_metrics(df: pd.DataFrame) -> None:
+    columns = df.select_dtypes(include='number').columns
+    column = st.selectbox('Column', options=columns)
+    column = df[column]
+    col1, col2, col3, col4, col5 = st.columns(5)
+    col1.metric("Mean", column.mean())
+    col2.metric("Max", column.max())
+    col3.metric("Min", column.min())
+    col4.metric("Std", column.std())
+    col5.metric("Count", int(column.count()))
