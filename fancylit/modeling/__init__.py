35ad2a1172cff2b5a36e49d7ff9fb933a7471ac1
Pedro Gengo <pedro.gabriel.lourenco@hotmail.com>
2021-10-18 20:22:22 -0300

    Feature: Prediction error plot

--- fancylit/modeling/__init__.py
+++ fancylit/modeling/__init__.py
@@ -1,1 +1,1 @@
-from .yellowbrick_funcs import show_classification_report, feature_correlation, class_balance, umap_viz
+from .yellowbrick_funcs import show_classification_report, feature_correlation, class_balance, umap_viz, prediction_error
